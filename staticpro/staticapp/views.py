0313390a01288701a92fc9437ed924faa6d24cec
Ganesh Nagnath Murkute <ganeshmurkute021@gmail.com>
2026-01-06 08:35:18 +0530

    second commit by Zaif

--- staticpro/staticapp/views.py
+++ staticpro/staticapp/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def staticdata(request):
     print("hhelo")
+    print('bye')
     my_d={'name':'Sidhesh','loc':'pune','age':22}
     return render(request,'testapp/index.html',my_d)
