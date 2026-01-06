98ad10ff3bcd2e335f4d2e9557da74f1c4b987ea
Ganesh Nagnath Murkute <ganeshmurkute021@gmail.com>
2026-01-06 08:01:04 +0530

    commit by Ajeet

--- staticpro/staticapp/views.py
+++ staticpro/staticapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def staticdata(request):
+    print("hhelo")
     my_d={'name':'Sidhesh','loc':'pune','age':22}
     return render(request,'testapp/index.html',my_d)
